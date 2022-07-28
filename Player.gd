527cbe19799ec3f79f048f3384817956fd19bd63
Davi Souza <davi.souza@upperds.com>
2022-07-28 19:58:06 -0300

    frst

---
+++ Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+var curHp : int = 10
+var maxHp : int = 10
+var moveSpeed : int = 150
+var damage : int = 1
+var gold : int = 0
+var curLevel : int = 0
+var curXp : int = 0
+var xpToNextLevel : int = 50
+var xpToLevelIncreaseRate : float = 1.2
+var interactDist : int = 70
+var vel : Vector2 = Vector2()
+var facingDir : Vector2 = Vector2()
+onready var rayCast = get_node("RayCast2D")
+
+
+func _physics_process(delta):
+	vel = Vector2()
+	if Input.is_action_pressed("move_up"):
+		vel.y -= 1
+		facingDir = Vector2(0, -1)
+	if Input.is_action_pressed("move_down"):
+		vel.y += 1
+		facingDir = Vector2(0, 1)
+	if Input.is_action_pressed("move_left"):
+		vel.x -= 1
+		facingDir = Vector2(-1, 0)
+
+	if Input.is_action_pressed("move_right"):
+		vel.x += 1
+		facingDir = Vector2(1, 0)
+		
+	vel = vel.normalized()
+
+	move_and_slide(vel * moveSpeed)
